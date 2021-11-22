b7c02b10e328a06e1f4a9bf32a30d4fb64e6ee3e
samimeadad <sami.meadad@gmail.com>
2021-11-22 12:01:34 +0600

    queue overview done

--- queue.js
+++ queue.js
@@ -8,11 +8,33 @@ class Queue {
     enqueue ( element ) {
         this.queue.push( element );
     }
+
+    dequeue () {
+        this.queue.shift();
+    }
 }
 
 const invitation = new Queue();
 
+console.log( 'Adding', invitation.queue );
 invitation.enqueue( 'Sami' );
+console.log( 'Adding', invitation.queue );
 invitation.enqueue( 'Farmee' );
+console.log( 'Adding', invitation.queue );
 invitation.enqueue( 'Aarishamma' );
+console.log( 'Adding', invitation.queue );
 invitation.enqueue( 'Aarashabba' );
+console.log( 'Adding', invitation.queue );
+
+console.log( invitation.queue );
+
+invitation.dequeue();
+console.log( 'Removing', invitation.queue );
+invitation.dequeue();
+console.log( 'Removing', invitation.queue );
+invitation.dequeue();
+console.log( 'Removing', invitation.queue );
+invitation.dequeue();
+console.log( 'Removing', invitation.queue );
+invitation.dequeue();
+console.log( 'Removing', invitation.queue );
