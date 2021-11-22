1a9c840cc93ca37ad3c50f7fec7eec7ebdc1db70
samimeadad <sami.meadad@gmail.com>
2021-11-22 12:31:19 +0600

    linear search done

---
+++ dictionary.js
@@ -0,0 +1,13 @@
+class Dictionary {
+    constructor () {
+        this.dictionary = {};
+    }
+
+    add ( key, value ) {
+        this.dictionary[ key ] = value;
+    }
+
+    get ( key ) {
+        return this.dictionary[ key ];
+    }
+}
